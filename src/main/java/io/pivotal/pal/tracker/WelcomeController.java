853edfe441db4af22e43e9730d78ab929ad94b80
Maxwell J. Corbin <Maxwell.Corbin@cognizant.com>
2018-01-16 16:13:18 -0700

    test suite

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private String WELCOME_MESSAGE;
 
+    public WelcomeController(
+            @Value("${WELCOME_MESSAGE}") String WELCOME_MESSAGE
+    ) {
+        this.WELCOME_MESSAGE = WELCOME_MESSAGE;
+    }
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return WELCOME_MESSAGE;
     }
 }
